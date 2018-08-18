5160d057d3dc2ef4a7d547c7afaefc5a4691f076
Michal124 <gorskimichal27@gmail.com>
2018-08-18 11:20:13 +0200

    Add forward propagation

--- model_utils.py
+++ model_utils.py
@@ -40,12 +40,23 @@ def softmax(Z):
     
 
 # =============================================================================
-#                          Neural network functions
+#                          INITIALIZE PARAMETERS
 # =============================================================================
 
 
 def initialize_parameters(L_dims):
    
+    '''
+    Description: Initialize weight & bias for neural network
+    
+    Input : 
+        * L_dims -- array of layers ex. L_dims = [5,12,10,5]
+        
+    Output :
+        * parameters -- initialize parameters of wei==ghts and bias 
+     
+    '''
+    
     np.random.seed(3)
     parameters = {}
     L = len(L_dims)
@@ -58,10 +69,97 @@ def initialize_parameters(L_dims):
     return parameters
 
 
+# =============================================================================
+#                        Forward propagation
+# =============================================================================
    
+def forward_propagation(A_prev,W,b,activation):
+
+    '''
+    Description:  Compute linear and activation functions for each layer.
     
+    
+    Input:
+        * A_prev -- Previous layer activation or Input features
+        * W -- weights
+        * b -- bias
+        * activation -- activation function of previous layer 
+
+    Output:
+        * A -- activation function of next layer
+        * cache -- memory for parameters(A_prev,W,b) in each layer 
+        
+    '''
+    
+    #First step of neuron Computation (linear)
+    Z = np.dot(W.A_prev) + b  
+    
+    #Second step of neuron computation (activation) 
+    if activation == "relu":
+        A = relu(Z)
+    
+    elif activation == "leaky_relu":
+        A = leaky_realu(Z)
+    
+    elif activation == "tanh" :
+        A = tanh(Z)
+        
+    elif activation == "sigmoid": # binary output
+        A = sigmoid(Z)
+        
+   # elif activation == "softmax": # Categorical output
+   #     A = softmax(Z)
+    
+    
+    # Create linear and activation cache
+    
+    linear_cache = (A_prev,W,B)
+    activation_cache = Z
+    
+    # Add both steps of computation to cache
+    
+    cache = (linear_cache,activation_cache)
+    return A, cache
+
+
+def forward_model(X,parameters):
+    
+    caches = []
+    A = X 
+    L = len(parameters)/2
+    
+    # compute hidden layers activation
+    for l in (1,L):
+        A_prev = A 
+        A, cache = forward_propagation(A_prev,
+                                       parameters["W" + str(l)],
+                                       parameters["b" + str(l)],
+                                       activation = "relu")
+        caches.append(cache)
+    
+    
+    # compute output layer activation
+    AL, cache = forward_propagation(A,
+                                    parameters["W" + str(L)],
+                                    parameters["b" + str(L)],
+                                    activation = "sigmoid")
+    
+    caches.append(cache)
+    
+    return AL, caches
+
     
 def compute_cost(AL,Y):
+    
+    '''
+    Input:
+      * AL -- Activation value in last layer
+      
+      * Y -- Outputs .
+    
+    Output:
+      * cost -- result of cost function.
+    '''
     
     m = Y.shape()
     
